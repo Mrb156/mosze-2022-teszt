093a8c4fd32f28aca022a3b8322301024e325d08
Morvai Barna <morvaibarna@gmail.com>
2022-09-09 18:25:48 +0200

    hibak javitasa

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];//hibas nev megadasa
-    std::cout << '1-100 ertekek duplazasa'//sortores, es pontosvesszo hianya, illetve ' helyett " hasznalata
-    for (int i = 0;)//hianyzik, hogy meddig tartson a ciklus es, hogy mi a lepeskoz
+    int *b = new int[N_ELEMENTS];//hibas nev megadasa
+    std::cout << "1-100 ertekek duplazasa" << std::endl;//sortores, es pontosvesszo hianya, illetve ' helyett " hasznalata
+    for (int i = 0; i < N_ELEMENTS; i++)//hianyzik, hogy meddig tartson a ciklus es, hogy mi a lepeskoz
     {
-        b[i] = i * 2;//nem egytol szamol, hanem nullatol
+        b[i] = (i + 1) * 2;//nem egytol szamol, hanem nullatol
     }
-    for (int i = 0; i; i++)//hianyzik, hogy meddig tartson a ciklus
+    for (int i = 0; i < N_ELEMENTS; i++)//hianyzik, hogy meddig tartson a ciklus
     {
-        std::cout << "Ertek:"//eredmeny kiiratas-, sortores-, es pontosvesszo hianya
+        std::cout << "Ertek:" << b[i] << std::endl;//eredmeny kiiratas-, sortores-, es pontosvesszo hianya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;//atlagszamitasnal meg kell adni egy kezdo erteket, amibol kiindul
+    int atlag = 0;//atlagszamitasnal meg kell adni egy kezdo erteket, amibol kiindul
     for (int i = 0; i < N_ELEMENTS, i++)// vesszo helyett pontosvesszo
     {
-        atlag += b[i]//pontosvesszo hianya
+        atlag += b[i];//pontosvesszo hianya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    //pointer felszabaditasa
+    delete[] b;//pointer felszabaditasa
     return 0;
 }
