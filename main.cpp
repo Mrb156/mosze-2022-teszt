9db8481acafcecd5d694af2341bc9c5c406e7a74
Morvai Barna <morvaibarna@gmail.com>
2022-09-09 18:24:19 +0200

    hibak kommentelese

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];//hibas nev megadasa
+    std::cout << '1-100 ertekek duplazasa'//sortores, es pontosvesszo hianya, illetve ' helyett " hasznalata
+    for (int i = 0;)//hianyzik, hogy meddig tartson a ciklus es, hogy mi a lepeskoz
     {
-        b[i] = i * 2;
+        b[i] = i * 2;//nem egytol szamol, hanem nullatol
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)//hianyzik, hogy meddig tartson a ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"//eredmeny kiiratas-, sortores-, es pontosvesszo hianya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;//atlagszamitasnal meg kell adni egy kezdo erteket, amibol kiindul
+    for (int i = 0; i < N_ELEMENTS, i++)// vesszo helyett pontosvesszo
     {
-        atlag += b[i]
+        atlag += b[i]//pontosvesszo hianya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //pointer felszabaditasa
     return 0;
 }
